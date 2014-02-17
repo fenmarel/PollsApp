3dc4e0c8596e3238ad4b4803115669e162318b8e
Jonny P <jonny.p.tatoes@gmail.com>
2014-02-16 18:30:54 -0800

    cleaned up the mess that was the results method
    Also, it was incorrect.  I suspect '.where("questions.poll_id = ?", self.id)' had something to do with it... question.poll_id = (this)question.id?  Corrected to use the 'Solution to N + 1 queries problem' pattern from Rails Guides.

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,26 +7,11 @@ class Question < ActiveRecord::Base
   belongs_to :poll
 
   def results
-    select_sql = <<-SQL
-      answer_choices.*,
-      COUNT(responses.id) AS response_count
-    SQL
-
-    responses_joins_sql = <<-SQL
-      LEFT OUTER JOIN
-        responses ON answer_choices.id = responses.answer_choice_id
-    SQL
-
-    answer_choices = AnswerChoice
-      .select(select_sql)
-      .joins(:question)
-      .joins(responses_joins_sql)
-      .where("questions.poll_id = ?", self.id)
-      .group("answer_choices.id")
+    answer_choices = self.answer_choices.includes(:responses)
 
     {}.tap do |results|
       answer_choices.each do |answer_choice|
-        results[answer_choice.text] = Integer(answer_choice.response_count)
+        results[answer_choice.text] = answer_choice.responses.length
       end
     end
   end
